1c4f9b09b1594adb744e97fc4176dd553a0b5fc8
wangzhqa <wangzhqa@yonyou.com>
2019-05-21 14:23:59 +0800

    添加测试代码

---
+++ project_009_ufoc/src/client/nc/report/hbbb/test/TestWorkPlugin.java
@@ -0,0 +1,21 @@
+package nc.report.hbbb.test;
+
+import com.ufida.zior.console.ActionHandler;
+
+import nc.bs.framework.test.AbstractTestCase;
+ 
+import nc.vo.pub.pa.CurrEnvVO;
+
+public class TestWorkPlugin extends AbstractTestCase{
+	
+	public void testPlugin(){
+//		ActionHandler.exec(ReportImportWorkPlugin.class.getName(),
+//				"executeTask", new CurrEnvVO(), true);
+	}
+	
+	public void testMeasurePlugin(){
+//		ActionHandler.exec(ReportMeasureWorkPlugin.class.getName(),
+//				"executeTask", new CurrEnvVO(), true);
+	}
+
+}
