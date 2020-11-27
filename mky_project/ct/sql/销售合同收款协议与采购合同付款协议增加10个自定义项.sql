e8ea27160fa6b3750e38315ac5aa9cb515486e8d
wangzhqa <wangzhqa@yonyou.com>
2020-11-27 15:57:03 +0800

    添加字段
    Signed-off-by: wangzhqa <wangzhqa@yonyou.com>

---
+++ mky_project/ct/sql/销售合同收款协议与采购合同付款协议增加10个自定义项.sql
@@ -0,0 +1,23 @@
+--销售合同收款协议
+alter table ct_sale_payterm add vdef1 varchar2(101) ; 
+alter table ct_sale_payterm add vdef2 varchar2(101) ; 
+alter table ct_sale_payterm add vdef3 varchar2(101) ; 
+alter table ct_sale_payterm add vdef4 varchar2(101) ; 
+alter table ct_sale_payterm add vdef5 varchar2(101) ; 
+alter table ct_sale_payterm add vdef6 varchar2(101) ; 
+alter table ct_sale_payterm add vdef7 varchar2(101) ; 
+alter table ct_sale_payterm add vdef8 varchar2(101) ; 
+alter table ct_sale_payterm add vdef9 varchar2(101) ; 
+alter table ct_sale_payterm add vdef10 varchar2(101) ;
+
+--采购合同付款协议
+alter table ct_pu_payment add vdef1 varchar2(101) ; 
+alter table ct_pu_payment add vdef2 varchar2(101) ; 
+alter table ct_pu_payment add vdef3 varchar2(101) ; 
+alter table ct_pu_payment add vdef4 varchar2(101) ; 
+alter table ct_pu_payment add vdef5 varchar2(101) ; 
+alter table ct_pu_payment add vdef6 varchar2(101) ; 
+alter table ct_pu_payment add vdef7 varchar2(101) ; 
+alter table ct_pu_payment add vdef8 varchar2(101) ; 
+alter table ct_pu_payment add vdef9 varchar2(101) ; 
+alter table ct_pu_payment add vdef10 varchar2(101) ;
