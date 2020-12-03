bcd93b140d2688852b8c1dbe05f6abf6353d5a47
wangzhqa <wangzhqa@yonyou.com>
2020-12-03 09:12:39 +0800

    协议字段修改
    Signed-off-by: wangzhqa <wangzhqa@yonyou.com>

--- mky_project/ct/sql/销售合同收款协议与采购合同付款协议增加10个自定义项.sql
+++ mky_project/ct/sql/销售合同收款协议与采购合同付款协议增加10个自定义项.sql
@@ -1,23 +1,23 @@
---销售合同收款协议
-alter table ct_sale_payterm add vdef1 varchar2(101) ; 
-alter table ct_sale_payterm add vdef2 varchar2(101) ; 
-alter table ct_sale_payterm add vdef3 varchar2(101) ; 
-alter table ct_sale_payterm add vdef4 varchar2(101) ; 
-alter table ct_sale_payterm add vdef5 varchar2(101) ; 
-alter table ct_sale_payterm add vdef6 varchar2(101) ; 
-alter table ct_sale_payterm add vdef7 varchar2(101) ; 
-alter table ct_sale_payterm add vdef8 varchar2(101) ; 
-alter table ct_sale_payterm add vdef9 varchar2(101) ; 
-alter table ct_sale_payterm add vdef10 varchar2(101) ;
+﻿--销售合同收款协议
+alter table ct_sale_payterm add vbdef1 varchar2(101) ; 
+alter table ct_sale_payterm add vbdef2 varchar2(101) ; 
+alter table ct_sale_payterm add vbdef3 varchar2(101) ; 
+alter table ct_sale_payterm add vbdef4 varchar2(101) ; 
+alter table ct_sale_payterm add vbdef5 varchar2(101) ; 
+alter table ct_sale_payterm add vbdef6 varchar2(101) ; 
+alter table ct_sale_payterm add vbdef7 varchar2(101) ; 
+alter table ct_sale_payterm add vbdef8 varchar2(101) ; 
+alter table ct_sale_payterm add vbdef9 varchar2(101) ; 
+alter table ct_sale_payterm add vbdef10 varchar2(101) ;
 
 --采购合同付款协议
-alter table ct_pu_payment add vdef1 varchar2(101) ; 
-alter table ct_pu_payment add vdef2 varchar2(101) ; 
-alter table ct_pu_payment add vdef3 varchar2(101) ; 
-alter table ct_pu_payment add vdef4 varchar2(101) ; 
-alter table ct_pu_payment add vdef5 varchar2(101) ; 
-alter table ct_pu_payment add vdef6 varchar2(101) ; 
-alter table ct_pu_payment add vdef7 varchar2(101) ; 
-alter table ct_pu_payment add vdef8 varchar2(101) ; 
-alter table ct_pu_payment add vdef9 varchar2(101) ; 
-alter table ct_pu_payment add vdef10 varchar2(101) ;
+alter table ct_pu_payment add vbdef1 varchar2(101) ; 
+alter table ct_pu_payment add vbdef2 varchar2(101) ; 
+alter table ct_pu_payment add vbdef3 varchar2(101) ; 
+alter table ct_pu_payment add vbdef4 varchar2(101) ; 
+alter table ct_pu_payment add vbdef5 varchar2(101) ; 
+alter table ct_pu_payment add vbdef6 varchar2(101) ; 
+alter table ct_pu_payment add vbdef7 varchar2(101) ; 
+alter table ct_pu_payment add vbdef8 varchar2(101) ; 
+alter table ct_pu_payment add vbdef9 varchar2(101) ; 
+alter table ct_pu_payment add vbdef10 varchar2(101) ;
