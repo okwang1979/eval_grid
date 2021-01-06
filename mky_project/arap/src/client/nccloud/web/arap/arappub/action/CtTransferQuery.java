2e0fa1e61447e3a3c6a8d24cea3020aef2c6995b
xulink <xulink@yonyou.com>
2021-01-06 14:44:25 +0800

    收入确认单拉单
    Change-Id: I142e7e2b6f4539f7fef11594002f1cd0f6c8824f
    Signed-off-by: xulink <xulink@yonyou.com>

---
+++ mky_project/arap/src/client/nccloud/web/arap/arappub/action/CtTransferQuery.java
@@ -0,0 +1,98 @@
+package nccloud.web.arap.arappub.action;
+
+import nc.itf.arap.fieldmap.IBillFieldGet;
+import nc.itf.uap.pf.IPfExchangeService;
+import nc.ui.querytemplate.querytree.IQueryScheme;
+import nc.vo.ct.saledaily.entity.CtSaleBVO;
+import nc.vo.ct.saledaily.entity.SpecialAggCtSaleVO;
+import nc.vo.pf.change.ExchangeVO;
+import nc.vo.pub.AggregatedValueObject;
+import nc.vo.pub.CircularlyAccessibleValueObject;
+import nc.vo.pub.billtype.BilltypeVO;
+import nccloud.framework.core.exception.BusinessException;
+import nccloud.framework.core.exception.ExceptionUtils;
+import nccloud.framework.core.json.IJson;
+import nccloud.framework.service.ServiceLocator;
+import nccloud.framework.web.action.itf.ICommonAction;
+import nccloud.framework.web.container.IRequest;
+import nccloud.framework.web.container.SessionContext;
+import nccloud.framework.web.json.JsonFactory;
+import nccloud.framework.web.ui.pattern.billcard.SpecilAggBill;
+import nccloud.framework.web.ui.pattern.billgrid.BillGrid;
+import nccloud.framework.web.ui.pattern.billgrid.BillGridOperator;
+import nccloud.pubitf.arap.arappub.IArapPfDataCacheService;
+import nccloud.pubitf.platform.query.INCCloudQueryService;
+import nccloud.pubitf.platform.template.IAppTemplateAssignment;
+import nccloud.web.arap.arappub.Info.SearchInfo;
+import nccloud.web.arap.arappub.util.ArapBillScaleUtil;
+import nccloud.web.arap.arappub.util.ArapBillUIUtil;
+import nccloud.web.riart.billref.src.INccSrcBillReferQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * 转单界面查询
+ * 
+ * @author wangshyh
+ * 
+ */
+public class CtTransferQuery implements ICommonAction {
+
+	@Override
+	public Object doAction(IRequest request) {
+		BillGrid[] grids = null;
+		try {
+			// 获取前台json
+			String read = request.read();
+			IJson json = JsonFactory.create();
+			SearchInfo info = json.fromJson(read, SearchInfo.class);
+			// 转换成queryscheme
+			INCCloudQueryService queryutil = ServiceLocator.find(INCCloudQueryService.class);
+			IQueryScheme scheme = queryutil.convertCondition(info.getQueryInfo());
+			INccSrcBillReferQuery service = getService(info);
+			// 查询 参考nc.ui.arap.billref.F1QueryServiceImpl.queryByQueryScheme(IQueryScheme)
+			SpecilAggBill[] vos = (SpecilAggBill[]) service.querySrcBill(scheme, info.getSrc_billtype(), info.getDest_tradetype(), null);
+			if (vos != null && vos.length > 0) {
+				// 转换成dto
+				BillGridOperator operator = new BillGridOperator(getPriorTemplateID(info), info.getPageId());
+				operator.setTransFlag(false);
+				grids = new BillGrid[vos.length];
+				grids = operator.toBillGrids(vos);
+			}
+		} catch (Exception e) {
+			ExceptionUtils.wrapException(e);
+		}
+		return grids;
+	}
+
+	private String getPriorTemplateID(SearchInfo info) {
+		SessionContext context = SessionContext.getInstance();
+		String templetid = null;
+		if (info.getAppCode() != null && !info.getAppCode().equals(context.getAppcode())) {// 传递的应用编码与当前应用编码不一致时，以传递的应用编码为准
+			String pagecode = info.getPageId();
+			// 获取templetid
+			IAppTemplateAssignment service = ServiceLocator.find(IAppTemplateAssignment.class);
+			try {
+				templetid = service.getPriorTemplateID(context.getClientInfo().getUserid(), context.getClientInfo().getPk_group(), info.getAppCode(), pagecode);
+			} catch (Exception e) {
+				ExceptionUtils.wrapException(e);
+			}
+			return templetid;
+		}
+
+		return templetid;
+	}
+
+	private INccSrcBillReferQuery getService(SearchInfo info) throws Exception {
+		BilltypeVO vo = ServiceLocator.find(IArapPfDataCacheService.class).getBillType(info.getSrc_billtype());
+		String implclass  = "nccloud.web.arap.ctbill.impl.CtbillTransferQueryImpl";
+		// 实例化查询服务类
+		Class<?> cla = Class.forName(implclass);
+		INccSrcBillReferQuery service = (INccSrcBillReferQuery) cla.newInstance();
+		return service;
+	}
+
+}
---
+++ mky_project/arap/src/client/nccloud/web/arap/ctbill/impl/CtbillTransferQueryImpl.java
@@ -0,0 +1,51 @@
+package nccloud.web.arap.ctbill.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import nc.impl.pubapp.pattern.data.bill.BillLazyQuery;
+import nc.itf.arap.fieldmap.IBillFieldGet;
+import nc.itf.ct.saledaily.ISaledailyMaintain;
+import nc.ui.querytemplate.querytree.IQueryScheme;
+import nc.vo.ct.saledaily.entity.AggCtSaleVO;
+import nc.vo.pub.BusinessException;
+import nccloud.framework.service.ServiceLocator;
+import nccloud.framework.web.ui.pattern.billcard.SpecilAggBill;
+import nccloud.web.riart.billref.src.INccSrcBillReferQuery;
+
+public class CtbillTransferQueryImpl implements INccSrcBillReferQuery  {
+
+	@Override
+	public Object[] querySrcBill(IQueryScheme queryScheme, String srcbillOrTransType, String destbillOrTransType, String pkBusitype) throws BusinessException {
+
+		queryScheme.put(IBillFieldGet.PK_BUSITYPE, pkBusitype);
+//		IArapGatheringBillQueryService service = ServiceLocator.find(IArapGatheringBillQueryService.class);
+//		// 执行转单查询
+//		AggGatheringBillVO[] vos = (AggGatheringBillVO[]) service.queryViewVOsByScheme(queryScheme);
+		BillLazyQuery<AggCtSaleVO> query = new BillLazyQuery<AggCtSaleVO>(
+				AggCtSaleVO.class);
+		
+		AggCtSaleVO [] vos  = query.query(queryScheme, null);
+		List<SpecilAggBill> newvos = new ArrayList<>();
+		
+		List<String> pid = new ArrayList<>();
+		for(AggCtSaleVO vo : vos) {
+			String pk_ct_sale = vo.getParentVO().getPk_ct_sale();
+			pid.add(pk_ct_sale);
+		}
+		
+		ISaledailyMaintain service = (ISaledailyMaintain) ServiceLocator.find(ISaledailyMaintain.class);
+		
+		vos = service.queryCtApVoByIds(pid.toArray(new String[pid.size()]));
+		
+		for(AggCtSaleVO vo : vos) {
+			SpecilAggBill newvo = new SpecilAggBill();
+			newvo.setHead(vo.getParentVO());
+			newvo.setBodys(vo.getCtSaleBVO());
+			newvos.add(newvo);
+		}
+		
+		return newvos.toArray(new SpecilAggBill[vos.length] );
+	}
+
+}
